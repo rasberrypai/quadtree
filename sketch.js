6b2d594a1fca6d27a41e0a6184c4907305ce6012
ajaypai <ajaypai@cs.washington.edu>
2020-09-07 18:57:50 -0700

    visualizaiton

---
+++ sketch.js
@@ -0,0 +1,18 @@
+let canvasSize = 750;
+let quadtree;
+let pointCounter = 0;
+
+function setup(){
+  createCanvas(canvasSize,canvasSize);
+  background(50);
+
+  quadtree = new QuadTree(new Rect(0,0,canvasSize,canvasSize),4);
+}
+
+function draw(){
+  background(50);
+  if(pointCounter < 1000)
+    quadtree.insert({ position: createVector(random(canvasSize),random(canvasSize))});
+  quadtree.show();
+  pointCounter++;
+}
