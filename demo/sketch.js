5cd3ac4fef1da956853beeca217a4cd7fddc2134
ajaypai <ajaypai@cs.washington.edu>
2020-09-11 14:15:24 -0700

    directory update

--- demo/sketch.js
+++ demo/sketch.js
@@ -13,8 +13,6 @@ function draw(){
   background(50);
   if(pointCounter < 1000)
     quadtree.insert({ position: createVector(random(canvasSize),random(canvasSize))});
-  else
-    console.log(quadtree);
   quadtree.show();
   pointCounter++;
 }
